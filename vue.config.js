58d9f6a37bd43d104e30f929feb2503246959307
SeonyongPark <tony3337@naver.com>
2023-01-01 22:33:05 +0900

    2023-01-01

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 let path = require("path");
 __dirname = path.resolve();
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: path.resolve(__dirname, "C:/ssafy/workspace/new_gwan/whereismyhome_vue/whereismyhome_springboot_source/src/main/resources/static"),
-  publicPath: "/"
-});
