320a5edbe013ebac33a14e772489ca79e9fd20a9
SeonyongPark <tony3337@naver.com>
2023-01-02 09:54:05 +0900

    2023-01-02

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 let path = require("path");
 __dirname = path.resolve();
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  outputDir: path.resolve(
+    __dirname,
+    "dist"
+  ),
+  publicPath: "/dist",
+});
